7c0431254bcc3e03ccac8e926d3e6e82d6845b16
Justin Bellero <j.bellero@dreamrocksmarttech.com>
2016-11-17 22:39:09 -0500

    init files

---
+++ rps.js
@@ -0,0 +1,30 @@
+function game() {
+    var options = ["scissor", "paper", "rock"];
+
+    function shoot() {
+        var x = Math.floor(Math.random() * 3)
+        return options[x];
+    }
+
+    function report(hval, cval, wins) {
+        let man = document.getElementById("human-choice");
+        let mach = document.getElementById("comp-choice");
+
+        man.textContent = hval;
+        mach.textContent = cval;
+        document.getElementById("winner").textContent = wins;
+    }
+
+    var computer = shoot();
+    var human = prompt("Choose rock, paper, or scissor.")
+
+    if (computer === human) {
+        alert("Looks like a tie")
+    } else if ((computer === "rock" && human === "paper") || (computer === "paper" && human === "scissor") || (computer === "scissor" && human === "rock")) {
+        return report(human, computer, "Man"); // human wins
+    } else {
+        return report(human, computer, "Machine"); //computer wins;
+    }
+}
+
+game();
